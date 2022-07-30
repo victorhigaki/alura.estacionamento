ef176da90e5c6c659654f28a294ee5d8698b6ef3
victorhigaki <victorhigaki@outlook.com>
2022-07-30 13:00:16 -0300

    teste com a classe Patio

---
+++ Alura.Estacionamento.Testes/PatioTeste.cs
@@ -0,0 +1,27 @@
+﻿using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTeste
+    {
+        [Fact]
+        public void TestarFaturamento()
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // ACT
+            double faturamento = estacionamento.TotalFaturado();
+
+            // ASSERT
+            Assert.Equal(2, faturamento);
+        }
+
+    }
+}
