fe25b70a13c7bb42b25aa35cd5c0125f3385d945
victorhigaki <victorhigaki@outlook.com>
2022-07-30 21:43:01 -0300

    Utilizar Theory e InlineData para testar com um conjunto de veiculos

--- Alura.Estacionamento.Testes/PatioTeste.cs
+++ Alura.Estacionamento.Testes/PatioTeste.cs
@@ -23,5 +23,31 @@ namespace Alura.Estacionamento.Testes
             Assert.Equal(2, faturamento);
         }
 
+        [Theory]
+        [InlineData("André Slva", "ASD-1498", "Preto", "Gol")]
+        [InlineData("Jose Slva", "POL-9242", "Cinza", "Fusca")]
+        [InlineData("Maria Slva", "GDR-6524", "Azul", "Opala")]
+        [InlineData("Pedro Slva", "GDR-0101", "Azul", "Corsa")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario,
+                                                        string placa,
+                                                        string cor,
+                                                        string modelo)
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            var faturamento = estacionamento.TotalFaturado();
+
+            // Assert
+            Assert.Equal(2, faturamento);
+        }
     }
 }
